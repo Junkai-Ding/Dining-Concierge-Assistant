4c5e5ce7609f4591dd443de450860b2aba4f446b
Junkai Ding <114452933+Junkai-Ding@users.noreply.github.com>
2023-10-16 16:28:01 -0400

    Create LF0.py

---
+++ lambda_functions/LF0.py
@@ -0,0 +1,53 @@
+import boto3
+import json
+# Define the client to interact with Lex
+client = boto3.client('lexv2-runtime')
+
+def lambda_handler(event, context):
+
+    # change this to the message that user submits on 
+    # your website using the 'event' variable
+    msg_from_user = event['messages'][0]['unstructured']['text']
+
+    print(f"Message from frontend: {msg_from_user}")
+
+    # Initiate conversation with Lex
+    response = client.recognize_text(
+            botId='OTVKIE2FW3', # MODIFY HERE
+            botAliasId='S18WIAZRKL', # MODIFY HERE
+            localeId='en_US',
+            sessionId='testuser',
+            text=msg_from_user)
+    
+    msg_from_lex = response.get('messages', [])
+    if msg_from_lex: # if we have a response from lex
+        
+        print(f"Message from Chatbot: {msg_from_lex[0]['content']}")
+        print(response)
+        
+        # modify resp to send back the next question Lex would ask from the user
+        
+        # format resp in a way that is understood by the frontend
+        # HINT: refer to function insertMessage() in chat.js that you uploaded
+        # to the S3 bucket
+        
+        resp = {
+            'statusCode': 200,
+            'messages': [ 
+                {'type': "unstructured", 
+                'unstructured': {'text': msg_from_lex[0]['content']}
+                } 
+            ]
+        }
+
+    else: # if there's no message from lex
+        resp = {
+            'statusCode': 200,
+            'messages': [ 
+                {'type': "unstructured", 
+                'unstructured': {'text': 'I’m still under development. Please come back later.'}
+                } 
+            ]
+        }
+    return resp
+        
